555b48f4112d5d2a9f5eb52acc19982e31fb8510
Adam Lodowski <adamlodowski@gmail.com>
2019-04-11 17:25:26 +0100

    Refactor: Extracted errors to own method to keep code DRY

--- src/main/scala/com/company/library/Library.scala
+++ src/main/scala/com/company/library/Library.scala
@@ -2,21 +2,29 @@ package com.company.library
 
 class Library(val books: List[Book] = Books.all) {
 
+  def error(searchType: String): String = {
+    searchType match {
+      case "isbn" => "Sorry, there is no book with that ISBN number"
+      case "title" => "Sorry, there are no books containing that title"
+      case "author" => "Sorry, there are no books written by that author"
+    }
+  }
+
   def searchISBN(isbn: String): Book = {
     val result = books.find(book => book.ISBN == isbn).head
-    if(result == null) println("Sorry, there is no book with that ISBN number")
+    if(result == null) println(error("isbn"))
     result
   }
 
   def searchTitle(title: String): List[Book] = {
     val results = books.filter(_.title.contains(title))
-    if(results == null) println("Sorry, there are no books containing that title")
+    if(results == null) println(error("title"))
     results
   }
 
   def searchAuthor(name: String): List[Book] = {
     val results = books.filter(_.author.contains(name))
-    if(results == null) println("Sorry, there are no books written by that author")
+    if(results == null) println(error("author"))
     results
   }
 
